e368daa687e9b104a0292cbd863b2a8db6c3d709
funstra <founstra@gmail.com>
2022-03-04 00:22:00 +0100

    workness

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,9 @@ module.exports = config => {
   config.addWatchTarget("src/scss/**/*.scss");
   config.addWatchTarget("src/js/**/*.js");
 
+  config.addWatchTarget('src/assets')
+  config.addPassthroughCopy('src/assets')
+
   config.addTransform("htmlmin", async function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
     if (outputPath && outputPath.endsWith(".html")) {
