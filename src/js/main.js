195761322caf0cdae7401d2424292d5123dfed41
funstra <founstra@gmail.com>
2022-03-03 21:14:46 +0100

    hue select

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ document.getElementById("click-outside").onclick = e => {
   const { navState } = document.documentElement.dataset;
   document.documentElement.dataset.navState = navState ? "" : "open";
 };
+
+window.setColor = e => {
+  const v = e.target.value.toString();
+  localStorage.setItem("hue", v);
+  document.documentElement.style.setProperty("--hue", v);
+};
